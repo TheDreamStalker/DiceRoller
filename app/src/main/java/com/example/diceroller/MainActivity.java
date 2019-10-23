9a6656e617928d895dcad11cdf74266a911f4231
SamuelBarreto <sambf2000@hotmail.com>
2019-10-23 21:48:54 +0100

    Added score box

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -19,16 +19,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void on_button_click(View view) {
 
-        TextView tv = this.findViewById(R.id.textView);
+        //Variables for the number output, user's guess and score.
+        TextView tv = this.findViewById(R.id.textView) ;
         TextView guess = this.findViewById(R.id.user_guess);
-        
+        TextView score = this.findViewById(R.id.scoreID);
 
-
+        //Random number is generated.
         Random r = new Random();
         int number = r.nextInt (6);
 
 
         tv.setText(Integer.toString(number));
 
+
+        int n = Integer.valueOf(tv.getText().toString());
+        int userNumber = Integer.valueOf(guess.getText().toString());
+
+        //Compares if the user's guess is the same as the random no.
+        if (userNumber == n) {
+            int Score = Integer.valueOf(score.getText().toString());
+            int ScoreChecker = Score + 1;
+
+            score.setText(Integer.toString(ScoreChecker));
+
+            //Display congratulations.
+            Toast.makeText(getApplicationContext(), "Congratulations, you guessed it!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
