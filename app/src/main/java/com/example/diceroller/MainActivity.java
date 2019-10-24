55459fb09ada140f0c933b4475232eff94db67cf
SamuelBarreto <sambf2000@hotmail.com>
2019-10-24 09:44:17 +0100

    Added exception handling

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void on_button_click(View view) {
-
         //Variables for the number output, user's guess and score.
         TextView tv = this.findViewById(R.id.textView) ;
         TextView guess = this.findViewById(R.id.user_guess);
@@ -28,22 +27,26 @@ public class MainActivity extends AppCompatActivity {
         Random r = new Random();
         int number = r.nextInt (6);
 
-
         tv.setText(Integer.toString(number));
 
+        //Handle exceptions in case there is no input.
+        try{
+            int n = Integer.valueOf(tv.getText().toString());
+            int userNumber = Integer.valueOf(guess.getText().toString());
 
-        int n = Integer.valueOf(tv.getText().toString());
-        int userNumber = Integer.valueOf(guess.getText().toString());
+            //Compares if the user's guess is the same as the random no.
+            if (userNumber == n) {
+                int Score = Integer.valueOf(score.getText().toString());
+                int ScoreChecker = Score + 1;
 
-        //Compares if the user's guess is the same as the random no.
-        if (userNumber == n) {
-            int Score = Integer.valueOf(score.getText().toString());
-            int ScoreChecker = Score + 1;
+                score.setText(Integer.toString(ScoreChecker));
 
-            score.setText(Integer.toString(ScoreChecker));
-
-            //Display congratulations.
-            Toast.makeText(getApplicationContext(), "Congratulations, you guessed it!", Toast.LENGTH_SHORT).show();
+                //Display congratulations.
+                Toast.makeText(getApplicationContext(), "Congratulations, you guessed it!", Toast.LENGTH_SHORT).show();
+            }
+        }
+        catch(Exception e){
+            Toast.makeText(getApplicationContext(), "Please input your guess", Toast.LENGTH_SHORT).show();
         }
     }
 }
